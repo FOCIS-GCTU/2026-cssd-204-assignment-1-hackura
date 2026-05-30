eff739539ab8f0b5ad46340668ddac020a85aa8c
hackura <karlseyram18@gmail.com>
2026-05-30 22:44:10 +0000

    Finished first assigment

--- initials.py
+++ initials.py
@@ -2,6 +2,19 @@
 def main():
   pass #code goes here
 
-
+  print()
+  print("...DKS")
+  print()
+  print("...DDDDDDDDD..........KK.....KK........SSSSSSSSSSSS...........")
+  print("...DDDDDDDDDDD........KK.....KK........SSSSSSSSSSSSSS.........")
+  print("...DD       DD........KK....KK.........SSSSS..................")
+  print("...DD       DD........KK KK............SSSSS..................")
+  print("...DD        DD.......KKK..............SSSSSS.................")
+  print("...DD        DD.......KKK.................SSSSSSS.............")
+  print("...DD        DD.......KK KK................SSSSSSS............")
+  print("...DD       DD........KK....KK................SSSSSSSS........")
+  print("...DDDDDDDDDDD...**...KK.....KK...**...SSSSSSSSSSSSSSS...**...")
+  print("...DDDDDDDDD.....**...KK.....KK...**...SSSSSSSSSSSSSS....**...")
+  print()
 
 main()
